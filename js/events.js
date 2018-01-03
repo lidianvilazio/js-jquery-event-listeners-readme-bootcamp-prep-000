55215a2d6b15837ae609e91bb3fccd00ca3da86e
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-03 22:35:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,13 +3,38 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
 
 function getIt() {
-  S(document).on('click', function(key) {
+  $(document).on('click', function(key) {
     if(key) {
       alert("Hey!")
     }
+  })
+}
+
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(event) {
+    if(event.which === 71) {
+        alert('G')
+    }
+  })
+}
+
+
+function submitIt() {
+  $('form').on('submit', function(event) {
+    alert("Your form is going to be submitted now.")
   })
 }

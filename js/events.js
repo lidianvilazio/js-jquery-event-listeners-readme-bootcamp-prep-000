e802c8d7cf3fc5a93585e8674a09e7fc4bb1c9b0
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-03 22:34:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 getIt()
 frameIt()
 pressIt()
+submitIt()
 });
 
 function getIt() {
@@ -24,9 +25,16 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('#typing').on('keydown', function(key) {
-    if(key.which === 71) {
+  $('#typing').on('keydown', function(event) {
+    if(event.which === 71) {
         alert('G')
     }
+  })
+}
+
+
+function submitIt() {
+  $(form).on('submit', function(event) {
+    alert("Your form is going to be submitted now.")
   })
 }
